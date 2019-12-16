8bc1248b75236d8e66873b964d6b748b7b0ea663
Prashant singh <30694084+silnshadow@users.noreply.github.com>
2019-12-16 19:51:17 +0530

    Variable_conversion

---
+++ var.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i1 = int()
+d1 = float()
+s1 = str()
+# Read and save an integer, double, and String to your variables.
+i1 = int(input())
+d1 = float(input())
+s1 = input()
+
+#sum =
+# Print the sum of both integer variables on a new line.
+print (i1 + i)
+# Print the sum of the double variables on a new line.
+print (d1 + d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print (s + s1 )
